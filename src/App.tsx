f3bf2bfce987003c65ffb5ab2236175c8902553b
Akash P <aksdevac@gmail.com>
2021-02-03 18:11:42 +0530

    basic setup: add web3, walletconnect, react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h3>WalletConnect</h3>
+        <div className="my-2">
+          <Button>Connect Wallet</Button>
+        </div>
       </header>
     </div>
   );
